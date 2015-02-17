15e52ede98b6693eabf2d220f9df3dfbe0cc1c33
Kasper Pihl Tornøe <kasper@pihl.it>
2015-02-17 19:25:04 +0200

    Initial Commit

---
+++ SwipesMac/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SwipesMac
+//
+//  Created by Kasper Pihl Tornøe on 17/02/15.
+//  Copyright (c) 2015 Swipes ApS. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SwipesMac/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SwipesMac
+//
+//  Created by Kasper Pihl Tornøe on 17/02/15.
+//  Copyright (c) 2015 Swipes ApS. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
